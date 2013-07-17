267f52c819401a6b13e5bcac0a96620e9d61167d
ammu <ponaruna@gmail.com>
2013-07-17 15:10:16 -0700

    testng file

--- src/calculate.java
+++ src/calculate.java
@@ -1,10 +1,22 @@
+import org.testng.annotations.Test;
+
+
+
 
 
 
 public class calculate {
-	public static void main(String args[]){
-		int a =30;
-		int b=20;
+	//public static void main(String args[]){
+	//	int a =30;
+	//	int b=20;
+	//	System.out.println(a+b);
+	//}
+	
+	@Test
+	public void perform(){
+		int a =10;
+		int b=2;
 		System.out.println(a+b);
 	}
 }
+
