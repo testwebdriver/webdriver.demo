421a014659827475c1c243b6e772f5f051dcbbc9
ammu <ponaruna@gmail.com>
2013-07-17 13:55:09 -0700

    division feature added

--- src/calculate.java
+++ src/calculate.java
@@ -1,13 +1,13 @@
 
 public class calculate {
 	public static void main(String args[]){
-		int a=5, b=10;
+		int a=50, b=10;
 		int result;
 		//result = a+b;
 		System.out.println(a+b);
 		System.out.println(a-b);
 	    System.out.println(a*b);
-		
+		System.out.println(a/b);
 		
 		
 	}
