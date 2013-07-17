5e162b5fabf26ff50258733e9208231fa30967e7
ammu <ponaruna@gmail.com>
2013-07-17 13:39:16 -0700

    added subtraction feature

---
+++ src/calculate.java
@@ -0,0 +1,15 @@
+
+public class calculate {
+	public static void main(String args[]){
+		int a=5, b=10;
+		int result;
+		//result = a+b;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		
+		
+		
+		
+	}
+
+}
